844266b05ef0c8ce648b6b7e3df6723f2838e86e
German Aguirre <w.aguirre@udla.edu.co>
2017-10-10 23:11:48 -0500

    ultimo 10-10

---
+++ VetOnlineBeta/Controllers/razasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VetOnlineBeta;
+
+namespace VetOnlineBeta.Controllers
+{
+    public class razasController : Controller
+    {
+        private vetonline3Entities db = new vetonline3Entities();
+
+        // GET: razas
+        public ActionResult Index()
+        {
+            var raza = db.raza.Include(r => r.tipomascota);
+            return View(raza.ToList());
+        }
+
+        // GET: razas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            raza raza = db.raza.Find(id);
+            if (raza == null)
+            {
+                return HttpNotFound();
+            }
+            return View(raza);
+        }
+
+        // GET: razas/Create
+        public ActionResult Create()
+        {
+            ViewBag.fkTipoMasc = new SelectList(db.tipomascota, "idTipoMascota", "descTipoMasc");
+            return View();
+        }
+
+        // POST: razas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idRaza,fkTipoMasc,DescRaza")] raza raza)
+        {
+            if (ModelState.IsValid)
+            {
+                db.raza.Add(raza);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.fkTipoMasc = new SelectList(db.tipomascota, "idTipoMascota", "descTipoMasc", raza.fkTipoMasc);
+            return View(raza);
+        }
+
+        // GET: razas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            raza raza = db.raza.Find(id);
+            if (raza == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.fkTipoMasc = new SelectList(db.tipomascota, "idTipoMascota", "descTipoMasc", raza.fkTipoMasc);
+            return View(raza);
+        }
+
+        // POST: razas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idRaza,fkTipoMasc,DescRaza")] raza raza)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(raza).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.fkTipoMasc = new SelectList(db.tipomascota, "idTipoMascota", "descTipoMasc", raza.fkTipoMasc);
+            return View(raza);
+        }
+
+        // GET: razas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            raza raza = db.raza.Find(id);
+            if (raza == null)
+            {
+                return HttpNotFound();
+            }
+            return View(raza);
+        }
+
+        // POST: razas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            raza raza = db.raza.Find(id);
+            db.raza.Remove(raza);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VetOnlineBeta/Controllers/vacunasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VetOnlineBeta;
+
+namespace VetOnlineBeta.Controllers
+{
+    public class vacunasController : Controller
+    {
+        private vetonline3Entities db = new vetonline3Entities();
+
+        // GET: vacunas
+        public ActionResult Index()
+        {
+            return View(db.vacunas.ToList());
+        }
+
+        // GET: vacunas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            vacunas vacunas = db.vacunas.Find(id);
+            if (vacunas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacunas);
+        }
+
+        // GET: vacunas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: vacunas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idVacuna,fkTipoEnfermedad,descVacuna")] vacunas vacunas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.vacunas.Add(vacunas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vacunas);
+        }
+
+        // GET: vacunas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            vacunas vacunas = db.vacunas.Find(id);
+            if (vacunas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacunas);
+        }
+
+        // POST: vacunas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idVacuna,fkTipoEnfermedad,descVacuna")] vacunas vacunas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vacunas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vacunas);
+        }
+
+        // GET: vacunas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            vacunas vacunas = db.vacunas.Find(id);
+            if (vacunas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacunas);
+        }
+
+        // POST: vacunas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            vacunas vacunas = db.vacunas.Find(id);
+            db.vacunas.Remove(vacunas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
